1a6006c35a040b7b3d48c6467133c32188647197
van <ironvanxv@gmail.com>
2016-12-21 08:00:57 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end
+  def join!(group)
+    participated_groups << group
+  end
+
+  def qiut!(group)
+    participated_groups.delete(group)
+  end
 end
